a71ec23b1efdd1a78001db05c5babbb11679760d
Mahith-Innovus <mahithr@innovus.lk>
2024-07-03 12:31:43 +0530

    changed history functions

--- routes/getHistory.js
+++ routes/getHistory.js
@@ -3,16 +3,35 @@ const connection = require('../database/connect');
 
 const router = express.Router();
 
-
 router.post('/getHistory', (req, res) => {
-    const { startDate, endDate } = req.body;
-    const query = `
-        SELECT DATE(timestamp) as date, SUM(pieceCount) as pieceCount
-        FROM pieceCount
-        WHERE DATE(timestamp) BETWEEN ? AND ?
-        GROUP BY DATE(timestamp)
-        ORDER BY DATE(timestamp);
-    `;
+    const { startDate, endDate, sortBy } = req.body;
+
+    let query;
+    if (sortBy === 'operation') {
+        query = `
+            SELECT DATE(timestamp) as date, operation, SUM(pieceCount) as pieceCount
+            FROM pieceCount
+            WHERE DATE(timestamp) BETWEEN ? AND ?
+            GROUP BY DATE(timestamp), operation
+            ORDER BY DATE(timestamp), operation;
+        `;
+    } else if (sortBy === 'plantName') {
+        query = `
+            SELECT DATE(timestamp) as date, plantName, SUM(pieceCount) as pieceCount
+            FROM pieceCount
+            WHERE DATE(timestamp) BETWEEN ? AND ?
+            GROUP BY DATE(timestamp), plantName
+            ORDER BY DATE(timestamp), plantName;
+        `;
+    } else {
+        query = `
+            SELECT DATE(timestamp) as date, SUM(pieceCount) as pieceCount
+            FROM pieceCount
+            WHERE DATE(timestamp) BETWEEN ? AND ?
+            GROUP BY DATE(timestamp)
+            ORDER BY DATE(timestamp);
+        `;
+    }
 
     connection.query(query, [startDate, endDate], (err, results) => {
         if (err) {
